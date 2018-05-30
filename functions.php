4672f042f1c0b08a46f013b874c4ab3533f367e1
Jeremy Bobbin <jeremybobbin@gmail.com>
2018-05-30 14:43:58 -0700

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,77 +1,31 @@
 <?php
 
-//Returns array of users
-function getAllUsers() {
-  $sqlString = "SELECT username FROM users";
-  include 'connection.php';
-  try {
-    $results = $db->query($sqlString);
-    return ($results->fetchAll(PDO::FETCH_COLUMN, 0));
-  } catch(Exception $e) {
-    echo $e->getMessage();
+function isLoggedIn() {
+  return (isset($_COOKIE['username']) && $_COOKIE['username']);
+}
+
+function renderLogButton() {
+  if(isLoggedIn()) {
+    $html = '"logout.php">Log Out';
+  } else {
+    $html = '"login.php">Log In';
+  }
+  return "<a class='nav-link' href=$html</a>";
+}
+
+function renderContent() {
+  $page = isset( $_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : 'home';
+  $path = '/content' . substr($page, strpos($page, '/', 1));
+
+
+  if (file_exists(filter_var($path, FILTER_SANITIZE_URL))) {
+      include $path;
+  } else {
+      echo $page. "    !    ";
+      echo $path;
   }
 }
 
-function createUser($username) {
-  $sqlString = 'INSERT INTO users (username) VALUES ' . "('$username');";
-  include 'connection.php';
-  try {
-    $db->query("$sqlString");
-    return true;
-  } catch(Exception $e) {
-    echo $e->getMessage();
-    return false;
-  }
+function run() {
+  include 'template/template.php';
 }
-
-function userExists($username) {
-  $sqlString = "SELECT 'id' FROM users WHERE username = '$username'";
-  include 'connection.php';
-  try {
-    $results = $db->query($sqlString);
-    if ($results->fetch(PDO::FETCH_ASSOC)) {
-      return true;
-    }
-    return false;
-  } catch(Exception $e) {
-    echo $e->getMessage() . $sqlString;
-  }
-}
-
-function getUserIdByName($username) {
-  var_dump($username);
-  include 'connection.php';
-  $sqlString = "SELECT id FROM users WHERE username = '$username'";
-  try {
-    $result = $db->query($sqlString);
-    return $result->fetch(PDO::FETCH_ASSOC)['id'];
-  } catch(Exception $e) {
-    echo $e->getMessage();
-  }
-}
-
-function postStatus($username, $text) {
-  include 'connection.php';
-  $userId = getUserIdByName($username);
-  $sqlString = 'INSERT INTO tweets (userId, text) VALUES' .
-     "('$userId', '$text')";
-  try {
-    $db->exec($sqlString);
-  } catch(Exception $e) {
-    echo $e->getMessage();
-  }
-}
-
-function getPostsByName($username) {
-  include 'connection.php';
-  $sqlString = "SELECT username, text, likes, date
-                FROM users JOIN tweets ON users.id = tweets.userID
-                WHERE users.username = '$username'";
-  try {
-    $results = $db->query($sqlString);
-    return $results->fetchAll(PDO::FETCH_ASSOC);
-  } catch (Exception $e) {
-    echo $e->getMessage();
-  }
-}
-?>
--- index.php
+++ index.php
@@ -1,29 +1,7 @@
 <?php
-include 'header.php';
-?>
-<a href="index.php" style="font-size: 5em;">Home</a>
-<?php
-if(isset($_COOKIE["username"]) && $_COOKIE["username"]) { ?>
-  <a href="logout.php">Log Out</a>
-  <form action="index.php" method="post">
-    <label for="status">
-      <textarea name="status" id="status"></textarea>
-      <button type="submit">Submit</button>
-    </label>
-  </form>
-<?php } else { ?>
-  <a href="login.php">Log In</a>
-  <a href="register.php">Register</a>
-<?php }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_COOKIE["username"]) {
-    include 'functions.php';
-    $cookie = $_COOKIE["username"];
-    postStatus($cookie, $_POST['status']);
-}
-?>
-<a href="users.php">Users</a>
-<a href="profile.php">Search a Guy.</a>
-<?php
-include 'footer.php';
+require 'functions.php';
+
+run();
+
 ?>
